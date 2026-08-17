2523a1675408b4a69c8feb3f19bf5f13491ce4f4
GuillhermeHenrique <piresguilherme003@gmail.com>
2026-08-17 10:31:22 -0300

    feat: remove rota de status e adiciona rota de inicio

--- app.py
+++ app.py
@@ -3,8 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def status():
-    return "Ok!"
+def inicio():
+    return {
+        "status": "ok",
+        "message": "Aplicação Flask funcionando",
+        "version": "1.0.0",
+        "environment": "development",
+        "data": {
+            "id": 8472,
+            "name": "OpsTrack",
+            "active": True
+        }
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
